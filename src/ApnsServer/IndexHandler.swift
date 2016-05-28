137b512e31d0565a60656c172f2bb309d99d2d12
Chris Barker <chris@calicoware.net>
2016-05-28 23:42:50 +0100

    first

--- src/ApnsServer/IndexHandler.swift
+++ src/ApnsServer/IndexHandler.swift
@@ -7,7 +7,7 @@
 //
 
 import PerfectLib
-import APNS
+//import APNS
 
 public func PerfectServerModuleInit(){
     
@@ -25,17 +25,17 @@ class IndexHandler: RequestHandler{
    
     func handleRequest(request: WebRequest, response: WebResponse) {
         
-        let aps = ["sound":"default", "alert":"testPush()"]
-        let payload = ["aps":aps]
-        try! APNSNetwork().sendPush("com.myapp.bundle",
-                                    priority: 10,
-                                    payload: payload,
-                                    deviceToken: "3dd55a59056441ab275b8b679458388cae76be3a9a02a00234388e50fe91f2fe",
-                                    certificatePath: NSBundle.mainBundle().pathForResource("push", ofType: "p12")!,
-                                    passphrase: "",
-                                    sandbox: true) { (response) -> Void in
-                                        
-        }
+//        let aps = ["sound":"default", "alert":"testPush()"]
+//        let payload = ["aps":aps]
+//        try! APNSNetwork().sendPush("com.myapp.bundle",
+//                                    priority: 10,
+//                                    payload: payload,
+//                                    deviceToken: "3dd55a59056441ab275b8b679458388cae76be3a9a02a00234388e50fe91f2fe",
+//                                    certificatePath: NSBundle.mainBundle().pathForResource("push", ofType: "p12")!,
+//                                    passphrase: "",
+//                                    sandbox: true) { (response) -> Void in
+//                                        
+//        }
         
         
         response.requestCompletedCallback()
